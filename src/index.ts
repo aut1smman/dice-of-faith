60eebd51369df1d8fdc95986b436e37577a5a1e9
aut1smman <braidgsava@gmail.com>
2022-08-28 15:54:23 +0500

    chandeg start speed

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import DiceOfFaith from './scripts/dice-of-faith';
 const diceOfFaith = new DiceOfFaith(0.01, new THREE.Vector3(0, 0, 10),
     5, 0xFA4D5F, 0xffaf85,
     ['Да', 'Нет', 'Возможно', 'Точно\nНет', 'Забудь', 'Даа!!!'],
-    1000);
+    5000);
 
 diceOfFaith.initialize();
 
